56c126b506cdb6d74740ba200ef339cb6158e63f
Justin Hager <developerjustinhager@pm.me>
2021-03-22 13:49:37 -0700

    Card component added

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './core/reportWebVitals';
 
-import { Content, Title } from './components';
+import { Content, Title, Card } from './components';
 import { GlobalStyles, theme } from './styles';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content>
-      <Title>Oh Yeah 😸 </Title>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku 😸 </Title>
+      <Card data-cy="card">This is a card section</Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')

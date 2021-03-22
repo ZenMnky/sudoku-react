33c7ae745a98edc5cf6cdb15718e8a7942261355
Justin Hager <developerjustinhager@pm.me>
2021-03-22 13:40:36 -0700

    add Title styled component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './core/reportWebVitals';
 
+import { Content, Title } from './components';
+import { GlobalStyles, theme } from './styles';
+import { ThemeProvider } from 'styled-components';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Oh Yeah 😸 </div>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content>
+      <Title>Oh Yeah 😸 </Title>
+    </Content>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
